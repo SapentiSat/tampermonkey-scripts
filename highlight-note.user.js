6687b8f8c6ec1bc6735c06f62d82fca1c4128e98
Valiantsin <107068296+SapentiSat@users.noreply.github.com>
2025-09-07 22:41:06 +0200

    Update highlight-note.user.js

--- highlight-note.user.js
+++ highlight-note.user.js
@@ -2,7 +2,7 @@
 // @name         Podświetl sekcję „Notatka wewnętrzna”
 // @namespace    suuhouse.tools
 // @author       valiantsin12@gmail.com
-// @version      1.3
+// @version      1.4
 // @description  Podświetla sekcję „Notatka wewnętrzna”
 // @match        https://suuhouse.enterprise.sellrocket.pl/unified-orders/*
 // @run-at       document-idle
