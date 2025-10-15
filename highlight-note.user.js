df51fd3f5c99b87260d59db062e2133de1554123
Valiantsin <107068296+SapentiSat@users.noreply.github.com>
2025-10-15 14:41:40 +0200

    Update highlight-note.user.js

--- highlight-note.user.js
+++ highlight-note.user.js
@@ -2,7 +2,7 @@
 // @name         Podświetl sekcję „-Notatka wewnętrzna-”
 // @namespace    suuhouse.tools
 // @author       valiantsin12@gmail.com
-// @version      1.4.9
+// @version      1.5.0
 // @description  Podświetla sekcję „Notatka wewnętrzna”
 // @match        https://suuhouse.enterprise.sellrocket.pl/unified-orders/*
 // @run-at       document-idle
@@ -13,8 +13,8 @@
 // @downloadURL  https://raw.githubusercontent.com/SapentiSat/tampermonkey-scripts/main/highlight-note.user.js
 // @grant        GM_getValue
 // @grant        GM_setValue
-// @grant        unsafeWindow
 // ==/UserScript==
+
 
 (function () {
   'use strict';
